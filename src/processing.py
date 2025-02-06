dafa504ecf512e550763497d35f19a966614d30c
Olga Python <olga_python@proton.me>
2025-02-06 23:03:52 +0100

    func filter_by_state added

---
+++ src/processing.py
@@ -0,0 +1,25 @@
+def filter_by_state(list_of_dicts: list[dict], key: str = "EXECUTED") -> list[dict]:
+    """Функция фильтрующая транзакции по статусу"""
+    selected_list_of_dicts = []
+    for dict in list_of_dicts:
+        if dict["state"] == key:
+            selected_list_of_dicts.append(dict)
+    return selected_list_of_dicts
+
+
+def sort_by_date():
+    pass
+
+
+if __name__ == "__main__":
+    print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                           {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                           {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                           {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],
+                          "EXECUTED"))
+
+    print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                           {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                           {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                           {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],
+                          "CANCELED"))
