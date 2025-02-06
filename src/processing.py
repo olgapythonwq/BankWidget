1083f8054bec266645a7dbd2e9024c65f45638f5
Olga Python <olga_python@proton.me>
2025-02-06 23:52:12 +0100

    func sort_by_date added

--- src/processing.py
+++ src/processing.py
@@ -7,8 +7,10 @@ def filter_by_state(list_of_dicts: list[dict], key: str = "EXECUTED") -> list[di
     return selected_list_of_dicts
 
 
-def sort_by_date():
-    pass
+def sort_by_date(list_of_dicts: list[dict]) -> list[dict]:
+    """Функция сортирующая транзакции по дате"""
+    sorted_list_of_dicts = sorted(list_of_dicts, key=lambda x: x["date"], reverse=True)
+    return sorted_list_of_dicts
 
 
 if __name__ == "__main__":
@@ -23,3 +25,8 @@ if __name__ == "__main__":
                            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
                            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],
                           "CANCELED"))
+
+    print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
