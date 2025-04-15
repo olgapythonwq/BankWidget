e2e9d3e497fa3e1d2ec1140460c7bef0aaa7adc5
Olga Python <olga_python@proton.me>
2025-04-15 23:12:54 +0200

    added functions to fin_transactions.py

--- src/fin_transactions.py
+++ src/fin_transactions.py
@@ -1,5 +1,7 @@
 import csv
 import os
+import re
+from collections import Counter
 
 import pandas as pd
 
@@ -7,6 +9,7 @@ from config import ROOT_DIR
 
 path_to_csv = os.path.join(ROOT_DIR, 'data', 'transactions.csv')
 path_to_xl = os.path.join(ROOT_DIR, 'data', 'transactions_excel.xlsx')
+path_to_json = os.path.join(ROOT_DIR, 'data', 'operations.json')
 
 
 def get_operations_from_csv(path: str) -> list[dict]:
@@ -22,8 +25,53 @@ def get_operations_from_xl(path: str) -> list[dict]:
     return df.to_dict(orient='records')  # преобразует данные из объекта DataFrame или Series в словарь
 
 
+def filter_transactions_by_description(transactions: list[dict], description) -> list[dict]:
+    """Функция, принимающая банковские операции и строку поиска, возвращающая те, у которых
+    в описании есть данная строка"""
+    pattern = re.compile(re.escape(description), re.IGNORECASE)  # .escape() - Экранирование спецсимволов
+    result = []
+    for transaction in transactions:
+        if not isinstance(transaction.get('description'), str):  # Если описание в транзакции отсутствует
+            continue
+        else:
+            if pattern.search(transaction.get('description')):
+                result.append(transaction)
+    return result
+
+
+def get_category_list(transactions: list[dict]) -> list[str]:
+    """Функция, принимающая банковские операции и возвращающая список категорий из description"""
+    category_list = []
+    for transaction in transactions:
+        if not isinstance(transaction.get('description'), str):  # Если описание в транзакции отсутствует
+            continue
+        else:
+            if transaction.get('description').lower() not in category_list:
+                category_list.append(transaction.get('description').lower())
+    return category_list
+
+
+def count_transactions_per_category(transactions: list[dict], category_list: list[str]) -> dict:
+    """Функция, принимающая банковские операции и список категорий операций, возвращающая словарь,
+    в котором ключи — это названия категорий, а значения — это количество операций в каждой категории"""
+    list_of_descriptions = []
+    for transaction in transactions:
+        if not isinstance(transaction.get('description'), str):  # Если описание в транзакции отсутствует
+            continue
+        else:
+            if transaction.get('description').lower() in category_list:
+                list_of_descriptions.append(transaction.get('description'))
+    counted = Counter(list_of_descriptions)
+    return counted
+
+
 # if __name__ == '__main__':
     # get_operations_from_csv(path_to_csv)
     # print(get_operations_from_csv(path_to_csv)[:5])
     # get_operations_from_csv(path_to_csv)
-    # print(get_operations_from_xl(path_to_xl))
+    # pprint(get_operations_from_xl(path_to_xl))
+    # pprint(filter_transactions_by_description(get_operations_from_xl(path_to_xl), 'счета'))
+    # print(get_category_list(get_operations_from_xl(path_to_xl)))
+    # print(count_transactions_per_category(get_operations_from_xl(path_to_xl),
+# get_category_list(get_operations_from_xl(path_to_xl))))
+    # pprint(get_operations_from_xl(path_to_xl))
